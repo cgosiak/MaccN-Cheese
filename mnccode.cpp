388152e03165c6e1957558567c1552dda1937e86
Caleb <clubs67charger@gmail.com>
2016-04-25 08:26:21 -0500

    updated

---
+++ mnccode.cpp
@@ -0,0 +1,356 @@
+/*	____________________________________________________________________________
+
+	    Semantics and Coding Component Implementation for the Micro Compiler
+
+	                               mcode.cpp
+
+	                              Version 2007
+ 
+	                           James L. Richards
+	                     Last Update: August 28, 2007
+	                     Update by M. J. Wolf: January 21,2016
+
+	The routines in this unit are based on those provided in the book 
+	"Crafting A Compiler" by Charles N. Fischer and Richard J. LeBlanc, Jr., 
+	Benjamin Cummings Publishing Co. (1991).
+
+	See Section 2.3-2.4, pp. 31-40.
+	____________________________________________________________________________
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+extern ifstream sourceFile;
+extern ofstream outFile, listFile;
+
+#include "mncscan.h"   // Scanner class definition
+#include "mnccode.h"
+#include "SymbolTable.h"
+
+extern Scanner scan; // global Scanner object declared in micro.cpp
+extern SymbolTable symbolTable;
+
+// *******************
+// **  Constructor  **
+// *******************
+
+CodeGen::CodeGen()
+{
+	maxTemp = 0;
+}
+
+// *******************************
+// ** Private Member Functions  **
+// *******************************
+
+void CodeGen::CheckId(const string & s)
+{
+	if (!LookUp(s))  // variable not declared yet
+		Enter(s);
+}
+
+void CodeGen::Enter(const string & s)
+{
+	symbolTable_Vector.push_back(s);
+}
+
+void CodeGen::ExtractExpr(const ExprRec & e, string& s)
+{
+	string t;
+	int k, n;
+
+	switch (e.kind)
+	{
+	case ID_EXPR:
+	case TEMP_EXPR:  // operand form: +k(R15)
+		s = e.name;
+		n = 0;
+		while (symbolTable_Vector[n] != s) n++;
+		k = 2 * n;  // offset: 2 bytes per variable
+		IntToAlpha(k, t);
+		s = "+" + t + "(R15)";
+		break;
+	case LITERAL_EXPR:
+		IntToAlpha(e.val, t);
+		s = "#" + t;
+	}
+}
+
+string CodeGen::ExtractOp(const OpRec & o)
+{
+	if (o.op == PLUS)
+		return "IA        ";
+	else
+		return "IS        ";
+}
+
+void CodeGen::Generate(const string & s1, const string & s2, const string & s3)
+{
+	listFile.width(20);
+	listFile << ' ' << s1;
+	outFile << s1;
+	if (s2.length() > 0)
+	{
+		listFile << s2;
+		outFile << s2;
+		if (s3.length() > 0)
+		{
+			listFile << ',' << s3;
+			outFile << ',' << s3;
+		}
+	}
+	listFile << endl;
+	outFile << endl;
+}
+
+string CodeGen::GetTemp()
+{
+	string s;
+	static string t;
+
+	t = "Temp&";
+	IntToAlpha(++maxTemp, s);
+	t += s;
+	CheckId(t);
+	return t;
+}
+
+void CodeGen::IntToAlpha(int val, string& str)
+{
+	int k;
+	char temp;
+
+	str = "";
+	if (val == 0) str = "0";
+	while (val > 0)
+	{
+		str.append(1, (char)(val % 10 + (int)'0'));
+		val /= 10;
+	}
+	k = int(str.length());
+	for (int i = 0; i < k/2; i++)
+	{
+		temp = str[i];
+		str[i] = str[k-i-1];
+		str[k-i-1] = temp;
+	}
+}
+
+bool CodeGen::LookUp(const string & s)
+{
+	for (unsigned i = 0; i < symbolTable_Vector.size(); i++)
+	if (symbolTable_Vector[i] == s)
+		return true;
+
+	return false;
+}
+
+// ******************************
+// ** Public Member Functions  **
+// ******************************
+
+void CodeGen::Assign(const ExprRec & target, const ExprRec & source)
+{
+	string s;
+
+	ExtractExpr(source, s);
+	Generate("LD        ", "R0", s);
+	ExtractExpr(target, s);
+	Generate("STO       ", "R0", s);
+}
+
+void CodeGen::Finish()
+{
+	string s;
+
+	listFile.width(6);
+	listFile << ++scan.lineNumber << "  " << scan.lineBuffer << endl;
+	Generate("HALT      ", "", "");
+	
+	// Output Symbol Table
+	Generate(symbolTable.FinishSymbolTable(),"","");
+
+	Generate("LABEL     ", "VARS", "");
+	IntToAlpha(int(2*(symbolTable_Vector.size()+1)), s);
+	Generate("SKIP      ", s, "");
+	outFile.close();
+	listFile << endl << endl;
+	listFile << " _____________________________________________\n";
+	listFile << " <><><><>   S Y M B O L   T A B L E   <><><><>\n"
+		<< endl;
+	listFile << " Relative" << endl;
+	listFile << " Address      Identifier" << endl;
+	listFile << " --------     --------------------------------" 
+		<< endl;
+	for (unsigned i = 0; i < symbolTable_Vector.size(); i++)
+	{
+		listFile.width(7);
+		listFile << 2*i << "       " << symbolTable_Vector[i] << endl;
+	}
+	listFile << " _____________________________________________" 
+		<< endl;
+	listFile << endl;
+	listFile << " Normal successful compilation." << endl;
+	listFile.close();
+}
+
+void CodeGen::Shout(Token type_used) {
+	string s;
+
+	if (!(symbolTable.EntryExists("DECS"))) {
+			symbolTable.AddEntry("DECS",TYPE_CHEESE_LIT);
+	}
+
+	switch (type_used) {
+		case CHEESE_LIT:
+			s = scan.stringBuffer.data();
+			break;
+		case INT_LIT:
+			IntToAlpha(atoi(scan.tokenBuffer.data()), s);
+			break;
+		case FLOAT_LIT:
+			IntToAlpha(atof(scan.tokenBuffer.data()), s);
+			break;
+	}
+	symbolTable.UpdateEntry("DECS",s);
+	Shout_Variable("DECS");
+}
+
+void CodeGen::Shout_Variable(std::string input_var) {
+	DataEntry cur_entry = symbolTable.GetDataObject(input_var);
+
+	switch (cur_entry.GetType()) {
+		case TYPE_CHEESE_LIT:
+			Generate("WRST      ", cur_entry.GetCurrentTempVar(), "");
+			break;
+		case TYPE_BOOL_LIT:
+		case TYPE_INT_LIT:
+			Generate("WRI       ", cur_entry.GetCurrentTempVar(), "");
+			break;
+		case TYPE_FLOAT_LIT:
+			Generate("WRF       ", cur_entry.GetCurrentTempVar(), "");
+			break;
+	}
+}
+
+void CodeGen::Listen(std::string input_var) {
+	symbolTable.UpdateEntry(input_var,"");
+
+	DataEntry cur_entry = symbolTable.GetDataObject(input_var);
+
+	switch (cur_entry.GetType()) {
+		case TYPE_CHEESE_LIT:
+			Generate("RDST      ", cur_entry.GetCurrentTempVar(), "");
+			break;
+		case TYPE_BOOL_LIT:
+		case TYPE_INT_LIT:
+			Generate("RDI       ", cur_entry.GetCurrentTempVar(), "");
+			break;
+		case TYPE_FLOAT_LIT:
+			Generate("RDF       ", cur_entry.GetCurrentTempVar(), "");
+			break;
+	}
+}
+
+void CodeGen::GenInfix(const ExprRec & e1, const OpRec & op, 
+                       const ExprRec & e2, ExprRec& e)
+{
+	string opnd;
+
+	if (e1.kind == LITERAL_EXPR && e2.kind == LITERAL_EXPR)
+	{
+		e.kind = LITERAL_EXPR;
+		switch (op.op)
+		{
+		case PLUS:
+			e.val = e1.val + e2.val;
+			break;
+		case MINUS:
+			e.val = e1.val - e2.val;
+		}
+	}
+	else
+	{
+		e.kind = TEMP_EXPR;
+		e.name = GetTemp();
+		ExtractExpr(e1, opnd);
+		Generate("LD        ", "R0", opnd);
+		ExtractExpr(e2, opnd);
+		Generate(ExtractOp(op), "R0", opnd);
+		ExtractExpr(e, opnd);
+		Generate("STO       ", "R0", opnd);
+	}
+}
+
+void CodeGen::NewLine()
+{
+	Generate("WRNL      ", "", "");
+}
+
+void CodeGen::ProcessId(ExprRec& e)
+{
+	CheckId(scan.tokenBuffer);
+	e.kind = ID_EXPR;
+	e.name = scan.tokenBuffer;
+}
+
+void CodeGen::ProcessLiteral(ExprRec& e)
+{
+	e.kind = LITERAL_EXPR;
+	e.val = atoi(scan.tokenBuffer.data());
+	// cout << scan.tokenBuffer.data() << endl;
+}
+
+void CodeGen::ProcessOp(OpRec& o)
+{
+	if (scan.tokenBuffer == "+")
+		o.op = PLUS;
+	else
+		o.op = MINUS;
+}
+
+void CodeGen::ReadId(const ExprRec & inVar)
+{
+	string s;
+
+	ExtractExpr(inVar, s);
+	Generate("RDI       ", s, "");
+}
+
+void CodeGen::Start()
+{
+	Generate("LDA       ", "R15", "VARS");
+}
+
+void CodeGen::WriteExpr(const ExprRec & outExpr)
+{
+	string s;
+
+	ExtractExpr(outExpr, s);
+	Generate("WRI       ", s, "");
+}
+
+void CodeGen::WriteString()
+{
+	string s;
+	s = scan.tokenBuffer.data();
+	Generate("WRST      ", s, "");
+}
+
+void CodeGen::Assign_Var2Var(std::string target, std::string source) {
+	DataEntry tar = symbolTable.GetDataObject(target);
+	DataEntry sou = symbolTable.GetDataObject(source);
+    if (tar.GetType() == TYPE_CHEESE_LIT) {
+        symbolTable.ReserveNewLabel(target);
+        tar = symbolTable.GetDataObject(target);
+        Generate("LDA       ", "R0", sou.GetCurrentTempVar());
+		Generate("LD        ", "R1", "#" + std::to_string(string_reservation_space));
+        Generate("BKT       ", "R0", tar.GetCurrentTempVar());
+    }
+    else {
+        Generate("LD        ", "R0", sou.GetCurrentTempVar());
+        Generate("STO       ", "R0", tar.GetCurrentTempVar());
+    }
+}
